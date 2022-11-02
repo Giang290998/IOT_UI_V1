9f82415d7036e636c591b1dfb3516c90eb933379
Giang98 <nguyenhoanggiang290998@gmail.com>
2022-11-03 00:29:00 +0700

    fix dang ki 03.11.22.00.28

--- src/pages/login_register/LoginRegister.jsx
+++ src/pages/login_register/LoginRegister.jsx
@@ -218,8 +218,7 @@ function LoginRegister() {
         }
     }
 
-    async function handleRegister(event) {
-        event.preventDefault()
+    async function handleRegister() {
         const dayOfBirthFormat = ("0" + dayOfBirth).slice(-2)
         const monthOfBirthFormat = ("0" + monthOfBirth).slice(-2)
         const dateOfBirth = `${yearOfBirth}-${monthOfBirthFormat}-${dayOfBirthFormat}`  
